fd9371d1baad2c9bc641341e50ed4609ac25c345
Andrew Thigpen <andyt05@gmail.com>
2023-06-11 16:25:27 -0400

    Initial infinite scroll attempt.
    Does not work well with window scrolling since the scroll position of
    the window stays the same when adding new elements to the page.

--- frontend/components/src/article_list.rs
+++ frontend/components/src/article_list.rs
@@ -1,7 +1,8 @@
 use log::info;
 use stores::article::ArticleStore;
+use web_sys::HtmlDivElement;
 use yew::prelude::*;
-use yew_hooks::{use_window_scroll, use_window_size};
+use yew_hooks::{use_debounce, use_window_scroll, use_window_size};
 use yewdux::prelude::*;
 
 use crate::article::Article;
@@ -12,21 +13,46 @@ pub fn article_list() -> Html {
 
     let (x, y) = use_window_scroll();
     let (w, h) = use_window_size();
+    let node = use_node_ref();
 
-    use_effect_with_deps(
-        move |_| {
-            dispatch.reduce_mut(move |s| s.fetch());
-        },
-        (),
-    );
+    {
+        let dispatch = dispatch.clone();
+        use_effect_with_deps(
+            move |_| {
+                dispatch.reduce_mut(move |s| s.fetch());
+            },
+            (),
+        );
+    }
 
-    use_effect(move || {
-        info!("x={} y={} w={} h={}", x, y, w, h);
-    });
+    let debouce = {
+        let dispatch = dispatch.clone();
+        use_debounce(
+            move || {
+                info!("should scroll debounced");
+                dispatch.reduce_mut(|s| s.fetch());
+            },
+            500,
+        )
+    };
+
+    {
+        let node = node.clone();
+        use_effect(move || {
+            let div = node.cast::<HtmlDivElement>().unwrap();
+            let offset_height = div.offset_height();
+            info!("x={} y={} w={} h={} offseth={}", x, y, w, h, offset_height);
+            if offset_height - (y as i32 + h as i32) < 150 {
+                info!("should scroll");
+                debouce.run();
+            }
+        });
+    }
 
     html! {
-        <div class={classes!("flex-grow", "flex", "flex-col", "max-w-4xl", "container",
-                             "rounded-lg", "dark:bg-slate-800")}>
+        <div ref={node} class={classes!(
+            "flex-grow", "flex", "flex-col", "max-w-4xl", "container", "rounded-lg", "dark:bg-slate-800"
+        )}>
             {for store.articles.iter().enumerate().map(|(idx, _)| html! { <Article index={idx} /> })}
         </div>
     }
--- frontend/stores/src/article.rs
+++ frontend/stores/src/article.rs
@@ -1,11 +1,16 @@
 use entity::article::Model as Article;
 use gloo_net::http::Request;
 use wasm_bindgen_futures::spawn_local;
-use yewdux::prelude::*;
+use yewdux::{log::info, prelude::*};
 
 #[derive(Debug, Clone, PartialEq, Eq, Store)]
 pub struct ArticleStore {
     pub articles: Vec<Article>,
+    // true when a fetch is currently in progress
+    pub fetching: bool,
+    // true when the last attempt to fetch articles returns less than the per_page amount...meaning
+    // no more articles are available
+    pub at_end: bool,
     pub per_page: u64,
     page: u64,
 }
@@ -14,6 +19,8 @@ impl Default for ArticleStore {
     fn default() -> Self {
         Self {
             articles: vec![],
+            fetching: false,
+            at_end: false,
             per_page: 20,
             page: 1,
         }
@@ -22,8 +29,12 @@ impl Default for ArticleStore {
 
 impl ArticleStore {
     pub fn fetch(&mut self) {
-        let page = self.page.to_string();
-        let per_page = self.per_page.to_string();
+        if self.fetching || self.at_end {
+            info!("fetching={} at_end={}", self.fetching, self.at_end);
+            return;
+        }
+        let page = self.page;
+        let per_page = self.per_page;
         spawn_local(async move {
             let dispatch = Dispatch::<ArticleStore>::new();
             let resp = Request::get("/api/articles")
@@ -48,7 +59,10 @@ impl ArticleStore {
             if let Ok(mut articles) = result {
                 dispatch.reduce_mut(|s| {
                     s.page += 1;
-                    s.articles.append(&mut articles)
+                    let len = articles.len();
+                    s.at_end = len as u64 != per_page;
+                    s.articles.append(&mut articles);
+                    s.fetching = false;
                 });
             }
         });
