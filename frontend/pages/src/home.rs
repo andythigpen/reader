00a1be6fbe1655ffe9dac58bf3d05c2560d4ab1e
Andrew Thigpen <andyt05@gmail.com>
2023-06-16 17:59:14 -0400

    Reload articles when the page reloads.

--- frontend/pages/src/home.rs
+++ frontend/pages/src/home.rs
@@ -1,3 +1,6 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
+use web_sys::{window, NavigationType, PerformanceNavigationTiming};
 use yew::prelude::*;
 use yewdux::prelude::*;
 
@@ -10,6 +13,32 @@ use stores::article::ArticleStore;
 #[function_component(Home)]
 pub fn home() -> Html {
     let fetching = use_selector(|s: &ArticleStore| s.fetching);
+
+    // reload the article store when the browser reloads the page
+    use_effect_with_deps(
+        {
+            move |_| {
+                let arr = window()
+                    .unwrap()
+                    .performance()
+                    .unwrap()
+                    .get_entries_by_type("navigation");
+                for elem in arr {
+                    let entry: PerformanceNavigationTiming = elem.unchecked_into();
+                    if entry.type_() == NavigationType::Reload {
+                        spawn_local(async move {
+                            Dispatch::<ArticleStore>::new()
+                                .reduce_mut_future(|s| Box::pin(async move { s.reload().await }))
+                                .await;
+                        });
+                    }
+                }
+                || ()
+            }
+        },
+        (),
+    );
+
     html! {
         <PageContainer>
             <PageContent>
